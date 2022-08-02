89ddc77631611f24f6842240a975b0308b889e3b
Erik Sommer <ersotech@posteo.de>
2022-08-02 08:54:04 +0200

    windows changes 2

--- setup.py
+++ setup.py
@@ -3,21 +3,25 @@ import os
 import subprocess
 from pathlib import Path
 
-WINDOWS = False
-if os.name == 'nt':
-    WINDOWS = True
-
-global_binary = "python" if WINDOWS else "python3"
-
 root_path = Path(__file__).parent
 venv_path = root_path.joinpath("venv")
 repo_path = root_path.joinpath("WS_THEbotIT")
 requirements_file = repo_path.joinpath("requirements-dev.txt")
 
+if os.name == "nt":
+    global_binary = "python"
+    local_binary = ".\\python"
+    local_binary_path = venv_path.joinpath("Scripts")
+else:
+    global_binary = "python3"
+    local_binary = "./python3"
+    local_binary_path = venv_path.joinpath("bin")
+
+subprocess.run([global_binary, "-m", "pip", "install", "virtualenv"])
 subprocess.run([global_binary, "-m", "venv", str(venv_path)])
 if not os.path.exists(repo_path):
     subprocess.run(["git", "clone", "git@github.com:the-it/WS_THEbotIT.git"], cwd=root_path)
 else:
     subprocess.run(["git", "pull"], cwd=repo_path)
-subprocess.run(["./python3", "-m", "pip", "install", "-r", requirements_file],
-               cwd=venv_path.joinpath("bin"))
+subprocess.run([local_binary, "-m", "pip", "install", "-r", requirements_file],
+               cwd=local_binary_path)
