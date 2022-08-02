424a901be4c966b4e9a71bb80d096e54e6afcfa5
Erik Sommer <ersotech@posteo.de>
2022-08-02 09:27:27 +0200

    windows changes 4

--- test.py
+++ test.py
@@ -6,9 +6,14 @@ from pathlib import Path
 root_path = Path(__file__).parent
 venv_path = root_path.joinpath("venv")
 
+if os.name == "nt":
+    local_binary_path = venv_path.joinpath("Scripts")
+else:
+    local_binary_path = venv_path.joinpath("bin")
+
 my_env = os.environ.copy()
 my_env["WS_REAL_DATA"] = "1"
 my_env["REGISTER_DATA_PATH"] = str(root_path)
-subprocess.run([venv_path.joinpath("bin").joinpath("nose2"), "--verbosity", "0"],
+subprocess.run([local_binary_path.joinpath("nose2"), "--verbosity", "0"],
                cwd=root_path,
                env=my_env)
