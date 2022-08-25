a9bc68f9234739e9254c9307de1c7034bb4e579d
Erik Sommer <ersotech@posteo.de>
2022-08-25 09:49:12 +0200

    use both requirements files

--- setup.py
+++ setup.py
@@ -6,7 +6,8 @@ from pathlib import Path
 root_path = Path(__file__).parent
 venv_path = root_path.joinpath("venv")
 repo_path = root_path.joinpath("WS_THEbotIT")
-requirements_file = repo_path.joinpath("requirements-dev.txt")
+requirements_file = repo_path.joinpath("requirements.txt")
+requirements_dev_file = repo_path.joinpath("requirements-dev.txt")
 
 if os.name == "nt":
     global_binary = "python"
@@ -23,5 +24,7 @@ if not os.path.exists(repo_path):
     subprocess.run(["git", "clone", "git@github.com:the-it/WS_THEbotIT.git"], cwd=root_path)  # nosec
 else:
     subprocess.run(["git", "pull"], cwd=repo_path)  # nosec
-subprocess.run([local_binary, "-m", "pip", "install", "-r", requirements_file],  # nosec
+subprocess.run([local_binary, "-m", "pip", "install",
+                "-r", requirements_file,
+                "-r", requirements_dev_file],  # nosec
                cwd=local_binary_path)
