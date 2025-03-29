4f8cf14d553763e699805e92d66c4ac30b6a9807
Erik Sommer <ersotech@posteo.de>
2025-03-29 17:25:32 +0100

    reset

---
+++ setup.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python3
+import os
+import subprocess  # nosec
+from pathlib import Path
+
+root_path = Path(__file__).parent
+venv_path = root_path.joinpath("venv")
+repo_path = root_path.joinpath("WS_THEbotIT")
+requirements_file = repo_path.joinpath("requirements.txt")
+requirements_dev_file = repo_path.joinpath("requirements-dev.txt")
+
+if os.name == "nt":
+    global_binary = "python"
+    local_binary_path = venv_path.joinpath("Scripts")
+    local_binary = local_binary_path.joinpath("python")
+else:
+    global_binary = "python3"
+    local_binary_path = venv_path.joinpath("bin")
+    local_binary = local_binary_path.joinpath("python3")
+
+subprocess.run([global_binary, "-m", "pip", "install", "virtualenv"])  # nosec
+subprocess.run([global_binary, "-m", "venv", str(venv_path)])  # nosec
+if not os.path.exists(repo_path):
+    subprocess.run(["git", "clone", "git@github.com:the-it/WS_THEbotIT.git"], cwd=root_path)  # nosec
+else:
+    subprocess.run(["git", "pull"], cwd=repo_path)  # nosec
+subprocess.run([local_binary, "-m", "pip", "install",
+                "-r", requirements_file,
+                "-r", requirements_dev_file],  # nosec
+               cwd=local_binary_path)
---
+++ test.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+import os
+import subprocess  # nosec
+from pathlib import Path
+
+root_path = Path(__file__).parent
+venv_path = root_path.joinpath("venv")
+
+if os.name == "nt":
+    local_binary_path = venv_path.joinpath("Scripts")
+else:
+    local_binary_path = venv_path.joinpath("bin")
+
+my_env = os.environ.copy()
+my_env["WS_REAL_DATA"] = "1"
+my_env["REGISTER_DATA_PATH"] = str(root_path)
+subprocess.run([local_binary_path.joinpath("nose2"), "--verbosity", "0"],  # nosec
+               cwd=root_path,
+               env=my_env)
